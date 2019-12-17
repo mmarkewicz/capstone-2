076ad68b1c72f1ca994b26afd6aa661edf59aab1
Matt M <42874279+mmarkewicz@users.noreply.github.com>
2019-12-17 15:39:23 -0500

    add swagger docs

--- inventory-service/src/main/java/com/company/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/company/inventoryservice/InventoryServiceApplication.java
@@ -2,8 +2,11 @@ package com.company.inventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
